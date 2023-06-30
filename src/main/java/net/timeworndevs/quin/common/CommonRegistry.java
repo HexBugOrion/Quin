1c4eb156816cb9d1e26dac8fc10b9bab4d64db72
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2023-06-29 22:26:40 -0500

    Signs are almost in working order

--- src/main/java/net/timeworndevs/quin/common/CommonRegistry.java
+++ src/main/java/net/timeworndevs/quin/common/CommonRegistry.java
@@ -1,12 +1,11 @@
 package net.timeworndevs.quin.common;
 
 
-import com.terraformersmc.terraform.sign.TerraformSign;
 import com.terraformersmc.terraform.sign.block.TerraformSignBlock;
+import com.terraformersmc.terraform.sign.block.TerraformWallSignBlock;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.advancement.criterion.FishingRodHookedCriterion;
 import net.minecraft.block.*;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.*;
@@ -16,7 +15,6 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.timeworndevs.quin.QuinMain;
 import net.timeworndevs.quin.block.TreeTapBlock;
-import org.apache.logging.log4j.core.net.MailManager;
 
 public class CommonRegistry {
 
@@ -48,8 +46,10 @@ public class CommonRegistry {
     public static final PressurePlateBlock DRIFTWOOD_PRESSURE_PLATE = new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings.of().mapColor(MapColor.OFF_WHITE), BlockSetType.CRIMSON);
     public static final ButtonBlock DRIFTWOOD_BUTTON = new ButtonBlock(FabricBlockSettings.of().mapColor(MapColor.OFF_WHITE).pistonBehavior(PistonBehavior.DESTROY), BlockSetType.CRIMSON, 30, true);
     //it's sign time
-    //public static final Identifier DRIFTWOOD_GUI_TEXTURE = Identifier.of(QuinMain.MODID, "textures/gui/sign/driftwood_sign");
-    //public static final TerraformSignBlock DRIFTWOOD_SIGN = new TerraformSignBlock(DRIFTWOOD_GUI_TEXTURE,FabricBlockSettings.of().mapColor(MapColor.OFF_WHITE));
+    public static final Identifier DRIFTWOOD_SIGN_TEXTURE = Identifier.of(QuinMain.MODID, "entity/signs/driftwood_sign");
+    public static final TerraformSignBlock DRIFTWOOD_SIGN = new TerraformSignBlock(DRIFTWOOD_SIGN_TEXTURE,FabricBlockSettings.of().mapColor(MapColor.OFF_WHITE).sounds(BlockSoundGroup.NETHER_WOOD).noCollision());
+    public static final TerraformWallSignBlock DRIFTWOOD_WALL_SIGN = new TerraformWallSignBlock(DRIFTWOOD_SIGN_TEXTURE, FabricBlockSettings.of().mapColor(MapColor.OFF_WHITE).sounds(BlockSoundGroup.NETHER_WOOD).noCollision().dropsLike(DRIFTWOOD_SIGN));
+    
     /*
         ItemGroup registry notes:
 
@@ -156,5 +156,9 @@ public class CommonRegistry {
         Registry.register(Registries.ITEM, new Identifier(QuinMain.MODID, "driftwood_button"), new BlockItem(DRIFTWOOD_BUTTON, new FabricItemSettings()));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {content.add(DRIFTWOOD_BUTTON);});
 
+        //sign
+        Registry.register(Registries.BLOCK, new Identifier(QuinMain.MODID, "driftwood_sign"), DRIFTWOOD_SIGN);
+        Registry.register(Registries.ITEM, new Identifier(QuinMain.MODID, "driftwood_sign"), new BlockItem(DRIFTWOOD_SIGN, new FabricItemSettings()));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {content.add(DRIFTWOOD_SIGN);});
     }
 }
