53a761e62fc0be4ffe5c0ff0b8a092d1391dc393
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2023-07-04 19:47:05 -0500

    e

---
+++ src/main/java/net/timeworndevs/quin/QuinClient.java
@@ -0,0 +1,19 @@
+package net.timeworndevs.quin;
+
+import com.terraformersmc.terraform.sign.SpriteIdentifierRegistry;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.render.TexturedRenderLayers;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.timeworndevs.quin.common.CommonBlockRegistry;
+
+public class QuinClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        initializeTextures();
+    }
+
+    private static void initializeTextures() {
+        SpriteIdentifierRegistry.INSTANCE.addIdentifier(new SpriteIdentifier(TexturedRenderLayers.SIGNS_ATLAS_TEXTURE, CommonBlockRegistry.DRIFTWOOD_SIGN.getTexture()));
+        SpriteIdentifierRegistry.INSTANCE.addIdentifier(new SpriteIdentifier(TexturedRenderLayers.SIGNS_ATLAS_TEXTURE, CommonBlockRegistry.DRIFTWOOD_HANGING_SIGN.getTexture()));
+    }
+}
